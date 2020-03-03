84ed7d130c855923a1057495eab20ba3789cfa0c
Travis Henson <travisghenson@gmail.com>
2020-03-03 01:18:41 -0600

    Updated home page with info and animations

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,15 +1,26 @@
 import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import { Helmet } from 'react-helmet';
+import './style.scss';
 
 const Home = () => {
   return (
-    <div>
+    <div className='home'>
       <Helmet>
         <title>Travis Henson | JavaScript Developer</title>
         <meta name='description' content='' />
         <meta name='keywords' content='' />
       </Helmet>
-      <h1>Hi, my name's Travis. I'm a JavaScript developer currently based in Pensacola, FL.</h1>
+      <h1>Hi, my name's <span className='blueText'>Travis Henson<span className='underline'></span></span>.</h1>
+      <h2>I'm a <span className='blueText'>JavaScript developer</span>, currently based in Pensacola, Florida.</h2>
+      <p>I love coding, and I enjoy building clean and easy-to-use React apps using Express and Node.js. I strive to be a life-long learner, and like many developers, have a 'Learning List' a mile long that I'll eventually get around to completing.</p>
+      <p>When I'm not coding, I enjoy watching sports of all kinds, playing video games on my PS4 and (nearly finished) custom-built PC, discovering new music on Spotify, and binging great TV shows with my wife.</p>
+      <div className='ctaLink'>
+        <Link to='/work'>
+          <span className='arrow'>></span> View My Work
+          <span className='underline'></span>
+        </Link>
+      </div>
     </div>
   )
 };
